700dcaff137f657b1f5b3d60a64ad820f65978a1
Zakaria Babahadji <zakaria.babahadji@gmail.com>
2024-06-20 14:51:47 -0400

    done with viz 1

--- src/server/bubble/chart.py
+++ src/server/bubble/chart.py
@@ -2,6 +2,7 @@ import plotly.graph_objects as go
 import plotly.io as pio
 import bubble.bubble as bubble
 import plotly.express as px
+import random
 def init_figure():
     '''
         Initializes the Graph Object figure used to display the bar chart.
@@ -28,39 +29,60 @@ def make_bubble_chart(df):
     fig = go.Figure()
     df['size'] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
     df['color'] = '#ff9999'
-    df['x'] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    df['y'] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    print(df)
+    random.seed(0)
+    df['x'] = generate_values(0, 200, 5, 15)
+    df['y'] = generate_values(0, 200, 5, 15)
+    df['text'] = df['label'] + '<br>' + df['value'].astype(str) 
     fig = px.scatter(
         df,
         x='x',
         y='y',
         size='size',
         color='color',
-        hover_name='label',
-        log_x=True,
-        log_y=True,
-        size_max=30,
+        hover_name='label',       
         color_discrete_sequence=px.colors.qualitative.Set1,
-        hover_data=['label', 'value']
-    )
-
-    # for label, value, size,color in zip(df['label'], df['value'], bubble.SIZES, bubble.COLORS):
-    #     fig.add_trace(go.Scatter(
-    #         x=[label],
-    #         y=[value],
-    #         mode='markers+text',
-    #         marker=dict(size=size, color=color, sizemode='area'),
-    #         text=f"{label}<br>{value}",
-    #         textposition="middle center",
-    #         showlegend=False
-    #     ))
+        hover_data=['label', 'value'],
+        size_max=200,
+        text='text' 
         
-    # fig.update_layout(
-    #     xaxis=dict(showgrid=False, zeroline=False),
-    #     yaxis=dict(showgrid=False, zeroline=False),
-    #     paper_bgcolor='rgba(0,0,0,0)',
-    #     plot_bgcolor='rgba(0,0,0,0)',     
-    # )
+    )
+  
+    fig.update_layout(
+        xaxis=dict(range=[-50, 250], 
+                   showgrid=False,
+                   showline=False, 
+                   zeroline=False, 
+                   showticklabels=False,
+                   ),
+        yaxis=dict(range=[-50, 250], 
+                   showgrid=False, 
+                   showline=False, 
+                   zeroline=False, 
+                   showticklabels=False, 
+                   ),
+        title='Bubble Chart',
+        template='plotly_dark',
+        showlegend=False
+    )
+    fig.update_traces(textposition='middle center')  
     
     return fig
+
+def generate_values(min_value, max_value, min_distance, max_distance):
+    values = [min_value]
+    current_value = min_value
+    use_min_distance = True
+
+    while True:
+        # Alternate between min_distance and max_distance
+        next_distance = min_distance if use_min_distance else max_distance
+        next_value = current_value + next_distance
+
+        if next_value > max_value:
+            break
+
+        values.append(next_value)
+        current_value = next_value
+        use_min_distance = not use_min_distance  # Alternate for next step
+    random.shuffle(values)
+    return values[:15]
