6f3c6a79481549b6debb8fd44c0bc9ba52046887
booskander <skander@Skanders-MacBook-Air.local>
2024-06-23 20:17:16 -0400

    saves

--- src/server/app.py
+++ src/server/app.py
@@ -1,9 +1,37 @@
 # app.py
 import dash
 import dash_html_components as html
-from flask import Flask, send_from_directory
-
+import dash_core_components as dcc
+from flask import Flask, send_from_directory, jsonify
+from server.bubble import chart, bubble, preprocess
+import plotly.io as pio
+import pandas as pd
 server = Flask(__name__)
+
+df_match_stats = pd.read_excel(
+    './assets/EURO_2020_DATA.xlsx', sheet_name='Match Stats')
+df_player_stats = pd.read_excel(
+    './assets/EURO_2020_DATA.xlsx', sheet_name='Players stats')
+df_viz_1 = preprocess.preprocess_data(df_match_stats, df_player_stats)
+# create_template()
+bubble_chart_figure = chart.init_figure()
+
+bubble_chart_figure = chart.make_bubble_chart(df_viz_1)
+
+bubble_graph = dcc.Graph(
+    id='bubble-chart',
+    figure=bubble_chart_figure
+)
+
+
+def to_html(component):
+    return pio.to_html(component, full_html=False, include_plotlyjs='cdn')
+
+
+@server.route('/pages/vis-1/bubble')
+def serve_bubble():
+    html = to_html(bubble_graph)
+    return jsonify(dict(bubble_graph=html))
 
 
 @server.route('/')
--- src/server/website/scripts/vis-1.js
+++ src/server/website/scripts/vis-1.js
@@ -3,3 +3,13 @@ const homeButton = document.querySelector("button");
 homeButton.onclick = () => {
     window.location.href = "/"
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/pages/vis-1/bubble')
+        .then(response => response.json())
+        .then(data => {
+            const dashContainer = document.getElementsByClassName("bubble-chart")[0];
+            dashContainer.appendChild(data.bubble_graph);
+        })
+        .catch(error => console.error('Error fetching bubble chart:', error));
+});
